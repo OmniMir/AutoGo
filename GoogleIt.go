816bad3c85ded899371a3c38e36492ec8a08f738
kapsilon <kapsilon47@gmail.com>
2018-01-11 18:56:08 +0300

    LIBS press + go binarries out

--- GoogleIt.go
+++ GoogleIt.go
@@ -4,7 +4,7 @@ import (
 	"os/exec"
 	//Libs for clipboard and
 	"github.com/atotto/clipboard"
-	"github.com/micmonay/keybd_event"
+	"libs"
 )
 
 const (
@@ -19,10 +19,7 @@ func main() {
 
 	clipboard.WriteAll(emptySearch)
 	//Copy selected text to clipboard
-	copyKey, _ := keybd_event.NewKeyBonding()
-	copyKey.SetKeys(keybd_event.VK_INSERT)
-	copyKey.HasCTRL(true)
-	copyKey.Launching()
+	libs.PressCopy()
 	//Read clipboard
 	wordsForSearch, _ := clipboard.ReadAll()
 	chromeSearchFinal := chromeSearch + wordsForSearch
---
+++ src/libs/press.go
@@ -0,0 +1,10 @@
+package libs
+
+import "github.com/micmonay/keybd_event"
+
+func PressCopy() {
+	copyKey, _ := keybd_event.NewKeyBonding()
+	copyKey.SetKeys(keybd_event.VK_INSERT)
+	copyKey.HasCTRL(true)
+	copyKey.Launching()
+}
