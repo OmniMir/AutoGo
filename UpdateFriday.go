9c943e47dd7c836ba79cf2ec2fa2828cf6aaf4ff
kapsilon <kapsilon47@gmail.com>
2019-01-04 00:36:06 +0300

    Kaspersky 18 -> 19
    Oh these updates

--- UpdateFriday.go
+++ UpdateFriday.go
@@ -9,7 +9,7 @@ const (
 	sumo      = "C:\\Program Files (x86)\\SUMo\\SUMo.exe" //winUpdate not work -> run application as document
 	winUpdate = "C:\\Users\\Sergey\\AppData\\Local\\Packages\\windows.immersivecontrolpanel_cw5n1h2txyewy\\LocalState\\Indexed\\Settings\\ru-RU\\AAA_SystemSettings_MusUpdate_UpdateActionButton.settingcontent-ms"
 	winStore  = "shell:Appsfolder\\Microsoft.WindowsStore_8wekyb3d8bbwe!App"
-	kaspersky = "C:\\Program Files (x86)\\Kaspersky Lab\\Kaspersky Internet Security 18.0.0\\avpui.exe"
+	kaspersky = "C:\\Program Files (x86)\\Kaspersky Lab\\Kaspersky Internet Security 19.0.0\\avpui.exe"
 	//steam = "C:\\Program Files (x86)\\Steam\\Steam.exe"
 
 	//Sites
