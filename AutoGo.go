dea138dbae2b2191e3a0a9e9e98759b49cfd93b7
kapsilon <kapsilon47@gmail.com>
2019-06-30 01:47:22 +0300

    Comics Increment
    Doctor Strange vol.1 #390 rus
    to
    Doctor Strange vol.1 #391 rus

--- AutoGo.go
+++ AutoGo.go
@@ -3,11 +3,14 @@ package main
 import (
 	. "github.com/OmniMir/AutoGo/libs"
 	"github.com/go-vgo/robotgo/clipboard"
+	"strconv"
+	"strings"
 )
 
 func main() {
 		googleIt()
 		operaIt()
+		comicsIncrement()
 	}
 
 }
@@ -31,4 +34,52 @@ func googleIt() {
 func operaIt() {
 	url, _ := clipboard.ReadAll()
 	StartOpera(url)
+}
+
+func comicsIncrement() {
+	const (
+		hashtag = "#"
+	)
+	//Getting existed information
+	existedName, _ := clipboard.ReadAll()
+	if !strings.Contains(existedName,hashtag){
+		return
+	}
+	existedTitle, existedNumber := stringInTwo(existedName, hashtag)
+
+	//Only Number
+	number := ""
+	appendix := ""
+	if strings.Contains(existedNumber,Space){
+		number, appendix = stringInTwo(existedNumber, Space)
+		appendix = Space + appendix
+	} else {
+		number = existedNumber
+	}
+
+	//Begin with Zero
+	prefix := ""
+	if strings.HasPrefix(number, "0") {
+		prefix = "0"
+	}
+
+	//Ranging
+	if strings.Contains(existedNumber,"*"){
+	//TODO
+	}
+
+	//Incrementing
+	numberInt, _ := strconv.Atoi(number)
+	numberInt = numberInt + 1
+	newNumber := strconv.Itoa(numberInt)
+
+	//New numbering
+	newTitleAndNumber := existedTitle + hashtag + prefix + newNumber + appendix
+	fmt.Println(newTitleAndNumber)
+	clipboard.WriteAll(newTitleAndNumber)
+}
+
+func stringInTwo (full string, delimiter string) (first string, second string) {
+	numberAndAppendix := strings.SplitN(full, delimiter, 2)
+	return numberAndAppendix[0], numberAndAppendix[1]
 }
