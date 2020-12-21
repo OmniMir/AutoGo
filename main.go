64424cb0f92fc30031133948457991844ee7ffd6
kapsilon <kapsilon47@gmail.com>
2020-12-21 19:14:04 +0300

    Update main.go

--- main.go
+++ main.go
@@ -8,17 +8,25 @@ import (
 func main() {
 	//Reading special flags
 	comics := flag.Bool("comics", false, "Comics Increment")
+	doublecmd := flag.Bool("doublecmd", false, "From Explorer to Double Commander")
+	edge := flag.Bool("edge", false, "From Chrome to Edge")
 	google := flag.Bool("google", false, "Call to Google")
 	movies := flag.Bool("movies", false, "Comics Increment")
 	opera := flag.Bool("opera", false, "From Chrome to Opera")
 	paradigm := flag.Bool("paradigm", false, "Paradigm Shift")
+	tagscanner := flag.Bool("tagscanner", false, "From Explorer to TagScanner")
 	terminal := flag.Bool("terminal", false, "Windows Terminal")
-	update := flag.Bool("update", false, "Comics Increment")
+	update := flag.Bool("update", false, "Update Your System")
+	winrar := flag.Bool("winrar", false, "From Explorer to WinRAR")
 	flag.Parse()
 
 	//Choosing right mode
 	if *comics {
 		ComicsIncrement()
+	} else if *doublecmd {
+		DoubleCommanderIt()
+	} else if *edge {
+		EdgeIt()
 	} else if *google {
 		GoogleIt()
 	} else if *movies {
@@ -27,12 +35,15 @@ func main() {
 		OperaIt()
 	} else if *paradigm {
 		ParadigmShift()
+	} else if *tagscanner {
+		TagScannerIt()
 	} else if *terminal {
-		Terminal()
+		TerminalIt()
 	} else if *update {
 		UpdateFriday()
+	} else if *winrar {
+		WinRarIt()
 	} else {
 		fmt.Println("You need right flag")
 	}
-
 }
