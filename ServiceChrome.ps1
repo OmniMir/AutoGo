13334f552a166aba0d634e808ec608412b9117b0
kapsilon <kapsilon47@gmail.com>
2020-12-21 19:13:25 +0300

    ADD Service Script Control
    PS to EXE

---
+++ ServiceChrome.ps1
@@ -0,0 +1,23 @@
+#Chrome Update Service
+$chromeUpdateService1 = "edgeupdate"
+$chromeUpdateService2 = "edgeupdatem"
+#Edge Update Service
+$edgeUpdateService1 = "gupdate"
+$edgeUpdateService2 = "gupdatem"
+
+if ((Get-Service gupdate).StartType -eq "Disabled") {
+	#Chrome Update Service
+	Set-Service -Name $chromeUpdateService1 -StartupType Manual
+	Set-Service -Name $chromeUpdateService2 -StartupType Manual
+	#Edge Update Service
+	Set-Service -Name $edgeUpdateService1 -StartupType Manual
+	Set-Service -Name $edgeUpdateService2 -StartupType Manual
+}
+else {
+	#Chrome Update Service
+	Set-Service -Name $chromeUpdateService1 -StartupType Disabled
+	Set-Service -Name $chromeUpdateService2 -StartupType Disabled
+	#Edge Update Service
+	Set-Service -Name $edgeUpdateService1 -StartupType Disabled
+	Set-Service -Name $edgeUpdateService2 -StartupType Disabled
+}
